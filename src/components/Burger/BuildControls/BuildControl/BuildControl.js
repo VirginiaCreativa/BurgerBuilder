6aa59f0d1f98d3e8f6a02ccbb667af74b2659e3b
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2018-08-10 10:13:58 -0500

    Outputting Multiple Build Controls

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,5 +1,11 @@
 import React from "react";
 import classes from "./BuildControl.scss";
 
-const buildControl = props => <div>Build Control Boton</div>;
+const buildControl = props => (
+  <div className={classes.CockpitControl}>
+    <label>{props.label}: </label>
+    <button className={classes.Less}>Less</button>
+    <button className={classes.More}>More</button>
+  </div>
+);
 export default buildControl;
--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -2,9 +2,22 @@ import React from "react";
 import classes from "./BuildControls.scss";
 import BuildControl from "./BuildControl/BuildControl.js";
 
+const controls = [
+  { label: "Meat", type: "meat" },
+  { label: "Cheese", type: "cheese" },
+  { label: "Salad", type: "salad" },
+  { label: "Bacon", type: "bacon" },
+  { label: "Onion", type: "onion" },
+  { label: "Tomats", type: "tomats" },
+  { label: "ketchup", type: "ketchup" },
+  { label: "Mustard", type: "mustard" },
+  { label: "Mayonnaise", type: "mayonnaise" }
+];
 const buildControls = props => (
   <div className={classes.CockpitControls}>
-    <BuildControl />
+    {controls.map(ctrl => {
+      return <BuildControl key={ctrl.label} label={ctrl.label} />;
+    })}
   </div>
 );
 export default buildControls;
